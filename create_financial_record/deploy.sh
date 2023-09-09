8d6eec933cadc2fba1d8a4bfc9ede6fa3e4076cc
Vishnu Sundaresan <68603272+juliussneezer04@users.noreply.github.com>
2023-09-09 15:59:50 -0400

    add SDK

--- create_financial_record/deploy.sh
+++ create_financial_record/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Compile the app
+leo build
+
+# Declare Variables
 WALLETADDRESS="aleo13u94svxm8arf7qncn8hj6ypaxcmcpkkkv3wrpgszsxmqfgdqmuys7z22ax"
 PRIVATEKEY="APrivateKey1zkpHD6U9E2NoPkDsHR6DynzUWm1Ns2n2NKsMkRT4NKk6uQK"
 
@@ -11,5 +16,6 @@ RECORD="{
   _nonce: 8062641925491950253687433891516803275378281494800078525450670532452506358456group.public
 }"
 
+# Deploy the app
 cd .. && snarkos developer deploy "${APPNAME}.aleo" --private-key "${PRIVATEKEY}" --query "https://vm.aleo.org/api" --path "./${APPNAME}/build/" --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast" --fee 1000000 --record "${RECORD}"``
 
