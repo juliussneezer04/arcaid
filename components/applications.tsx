837a9e2a12fb3d95224eae1e1fb18c555a7d4596
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2023-09-10 01:59:15 -0400

    add institution file

--- components/applications.tsx
+++ components/applications.tsx
@@ -5,24 +5,57 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import ClipLoader from "react-spinners/ClipLoader";
 
-import animation_lmd0dti7 from "../assets/animation_lmd0dti7.json"
+import animation_lmd0dti7 from "../assets/animation_lmd0dti7.json";
 import Lottie from "lottie-react";
 import { Box } from "@mui/material";
 
 type Application = {
-  studentIncome: number;
-  householdIncome: number;
-  expectedFamilyContribution: number;
+  studentIncome: string;
+  householdIncome: string;
+  expectedFamilyContribution: string;
+  institution: string;
   applicationHash: string;
 };
 
+const INSTITUTIONS = [
+  "University of Pennsylvania",
+  "Boston University",
+  "National University of Singapore",
+];
+
 export default function Applications() {
-  const [records, setRecords] = useState<Application[]>([]);
+  const [applications, setApplications] = useState<Application[]>([]);
+  const [institution, setInstitution] = useState("");
   const [open, setOpen] = useState(false);
   const [file, setFile] = useState<File>();
   const [processing, setProcessing] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getApplications = async () => {
+      setLoading(true);
+      const response = await fetch("/api/applications");
+      let userApplications: Application[] = await response.json();
+
+      userApplications = userApplications.map((application) => ({
+        studentIncome: "XXX",
+        householdIncome: "XXX",
+        expectedFamilyContribution: "XXX",
+        institution: application.institution,
+        applicationHash: application.applicationHash,
+      }));
+      setApplications(userApplications);
+      setLoading(false);
+    };
+
+    getApplications();
+  }, []);
 
   const addRecord = () => {
     setOpen(true);
@@ -40,23 +73,38 @@ export default function Applications() {
     };
     // TODO: call blockchain to create record
     setProcessing(true);
-    setTimeout(() => {
-      handleClose();
-      setRecords([
-        {
-          studentIncome: 5000,
-          householdIncome: 50000,
-          expectedFamilyContribution: 5000,
-          applicationHash: "0x123456789",
-        },
-      ]);
+    // TODO: call blockchain to get applicationHash
+
+    const newApplication: Application = {
+      studentIncome: "5000",
+      householdIncome: "50000",
+      expectedFamilyContribution: "5000",
+      institution: institution,
+      applicationHash: "0x123456789",
+    };
+
+    setTimeout(async () => {
+      await fetch("/api/applications", {
+        method: "POST",
+        body: JSON.stringify({
+          applicationHash: newApplication.applicationHash,
+          institution: newApplication.institution,
+        }),
+      });
+      setApplications([...applications, newApplication]);
       setProcessing(false);
-      setFile(undefined);
-    }, 10000);
+      handleClose();
+    }, 5000);
   };
 
   const handleClose = () => {
     setOpen(false);
+    setFile(undefined);
+    setInstitution("");
+  };
+
+  const handleInstitutionChange = (event: SelectChangeEvent) => {
+    setInstitution(event.target.value);
   };
 
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -77,15 +125,18 @@ export default function Applications() {
 
   return (
     <main>
-      <Dialog open={checkOpen} onClose={handleCheckClose} maxWidth={'sm'} fullWidth>
+      <Dialog
+        open={checkOpen}
+        onClose={handleCheckClose}
+        maxWidth={"sm"}
+        fullWidth
+      >
         <DialogTitle>Check Verification</DialogTitle>
         <DialogContent>
-          <Box maxWidth={'100px'}>
+          <Box maxWidth={"100px"}>
             <Lottie animationData={animation_lmd0dti7} loop={false} />
           </Box>
-          <DialogContentText>
-            Verified
-          </DialogContentText>
+          <DialogContentText>Verified</DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleCheckClose}>Close</Button>
@@ -123,11 +174,31 @@ export default function Applications() {
             </DialogContent>
 
             {file ? (
-              <DialogContent className="flex items-center justify-center">
+              <DialogContent className="flex-col items-center justify-center">
                 {processing ? (
                   <ClipLoader loading={processing} />
                 ) : (
-                  <DialogContentText>{file.name}</DialogContentText>
+                  <>
+                    <DialogContentText>{file.name}</DialogContentText>
+                    <FormControl fullWidth sx={{ marginTop: 2 }}>
+                      <InputLabel id="demo-simple-select-label">
+                        Institution
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={institution}
+                        label="institution"
+                        onChange={handleInstitutionChange}
+                      >
+                        {INSTITUTIONS.map((school, i) => (
+                          <MenuItem key={i} value={school}>
+                            {school}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                  </>
                 )}
               </DialogContent>
             ) : (
@@ -139,80 +210,89 @@ export default function Applications() {
 
             <DialogActions>
               <Button onClick={handleClose}>Cancel</Button>
-              {file && <Button onClick={handleSubmit}>Submit</Button>}
+              {file && institution && (
+                <Button onClick={handleSubmit}>Submit</Button>
+              )}
             </DialogActions>
           </Dialog>
 
-          <div className="mt-8 flow-root">
-            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead>
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
-                      >
-                        Student Income
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Household Income
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Expected Family Contribution
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Application Hash
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white">
-                    {records.length === 0 ? (
-                      <tr>
-                        <td colSpan={5} className="px-3 py-4 text-center text-sm text-gray-500">
-                          No rows available
-                        </td>
-                      </tr>
-                    ) : (
-                      records.map((record, index) => (
-                        <tr key={index}>
-                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
-                            {record.studentIncome}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            {record.householdIncome}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            {record.expectedFamilyContribution}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            {record.applicationHash}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            <button
-                              onClick={handleCheck}
-                              className="text-indigo-600 hover:text-indigo-900"
-                            >
-                              Check Eligibility
-                            </button>
-                          </td>
+          {loading ? (
+            <></>
+          ) : (
+            applications.length > 0 && (
+              <div className="mt-8 flow-root">
+                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                  <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <table className="min-w-full divide-y divide-gray-300">
+                      <thead>
+                        <tr>
+                          <th
+                            scope="col"
+                            className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
+                          >
+                            Student Income
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                          >
+                            Household Income
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                          >
+                            Expected Family Contribution
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                          >
+                            Institution
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                          >
+                            Application Hash
+                          </th>
                         </tr>
-                      ))
-                    )}
-                  </tbody>
-                </table>
+                      </thead>
+                      <tbody className="bg-white">
+                        {applications.map((record, index) => (
+                          <tr key={index}>
+                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-500 sm:pl-3">
+                              {record.studentIncome}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {record.householdIncome}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {record.expectedFamilyContribution}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {record.institution}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {record.applicationHash}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              <a
+                                href="#"
+                                className="text-indigo-600 hover:text-indigo-900"
+                              >
+                                Check Eligibility
+                              </a>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            )
+          )}
         </div>
       </div>
     </main>
